b5c5ef645e8f165170d7b70c520c94e4dca7592b
LuongVu307 <132726554+LuongVu307@users.noreply.github.com>
2023-12-11 20:03:32 +0000

    Update generator.py

--- generator.py
+++ generator.py
@@ -2,7 +2,7 @@ import pygame
 import random
 import sys
 import os
-from algorithm import Cell, RanDFS
+from algorithm import Cell, RanDFS, RanPrims, Eller, HuntKill, BinaryTree, RanKruskal
 
 pygame.init()
 
@@ -21,10 +21,22 @@ GRAY = (50, 50, 50)
 
 clock = pygame.time.Clock()
 
-grid_cell = [Cell(col, row) for row in range(rows) for col in range(cols)]
+grid_cell = [Cell(col, row, TILE, screen, cols, rows) for row in range(rows) for col in range(cols)]
+
+#current_cell = grid_cell[0]
+#stack = []
+
+#current_cell = grid_cell[rows*cols//2 + cols//2]
+#current_cell.visited = True
+
+#current_cell = grid_cell[0]
+#count = 0
+#set_cell = []
+
+#current_cell = grid_cell[0]
 
 current_cell = grid_cell[0]
-stack = []
+set_cell = []
 
 running = True
 while running:
@@ -37,7 +49,18 @@ while running:
 
   [cells.draw() for cells in grid_cell]
 
-  current_cell, stack = RanDFS(current_cell, stack, grid_cell)
+  #current_cell, stack = RanDFS(current_cell, stack, grid_cell)
+
+  #RanPrims(grid_cell)
+
+  #set_cell = Eller(grid_cell, rows, cols, count, set_cell)
+  #count += 1
+
+  #current_cell = HuntKill(current_cell, grid_cell)
+
+  #current_cell = BinaryTree(current_cell, grid_cell, cols)
+
+  set_cell = RanKruskal(grid_cell, set_cell)
 
   pygame.display.flip()
   clock.tick(FPS)
