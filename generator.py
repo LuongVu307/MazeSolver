a90acc1e3303ece66720d700c5b68ce7a5d41c02
LuongVu307 <132726554+LuongVu307@users.noreply.github.com>
2023-12-14 22:38:06 +0000

    Update generator.py

--- generator.py
+++ generator.py
@@ -22,30 +22,16 @@ GRAY = (50, 50, 50)
 
 clock = pygame.time.Clock()
 
-grid_cell = [Cell(col, row, TILE, screen, cols, rows) for row in range(rows) for col in range(cols)]
 
-#current_cell = grid_cell[0]
-#stack = []
-
-#current_cell = grid_cell[rows*cols//2 + cols//2]
-#current_cell.visited = True
-
-#current_cell = grid_cell[0]
-#count = 0
-#set_cell = []
-
-#current_cell = grid_cell[0]
-
-
-current_cell = grid_cell[0]
-set_cell = [set([i]) for i in range(len(grid_cell))]
-
-def draw_menu(option="1"):
+def draw_menu(page):
   
   screen.fill(WHITE)
 
   button = pygame.Rect(0, 0, 20, 20)
-  button.center = (GUI_WIDTH-30, GUI_HEIGHT//2)
+  if page == 1:
+    button.center = (GUI_WIDTH-30, GUI_HEIGHT//2)
+  elif page == 2:
+    button.center = (30, GUI_HEIGHT//2)
   pygame.draw.rect(screen, pygame.Color("black"), button)
   
   font = pygame.font.Font(None, 36)
@@ -55,50 +41,114 @@ def draw_menu(option="1"):
   option1 = ["DFS Maze", "Kruskal's Maze", "Prims Maze"]
   option2 = ["Eller Maze", "HuntKill Maze", "Binary Tree Maze"]
   option_width = 150
+
+  options = option1 if page == 1 else option2
   
-  for option in option1:
+  for option in options:
       option_text = font.render(option, True, BLACK)
       text_rect = option_text.get_rect(center=(option_width, GUI_HEIGHT//2))
+      choice = pygame.Rect(0, 0, 150, 150)
+      choice.center = (option_width, GUI_HEIGHT//2 + 100)
+      pygame.draw.rect(screen, pygame.Color("black"), choice)
       screen.blit(option_text, text_rect)
       option_width += 250
   
   pygame.display.flip()
   
   
-  
-
-
 
 
 
 def main():
   running = True
+  state = "menu"
+  page = 1
+  maze_type = None
   while running:
     for event in pygame.event.get():
       if event.type == pygame.QUIT:
         running = False
         sys.exit()
-  
+      elif event.type == pygame.MOUSEBUTTONDOWN:
+        if event.button == 1:
+          x, y = pygame.mouse.get_pos()
+          print(x, y)
+          if state == "menu":
+            if page == 1:
+              if 760 <= x <= 780 and 240 <= y <= 260:
+                page = 2
+              elif 75 <= x <= 225 and 275 <= y <= 425:
+                state="prepare"
+                maze_type = "DFS"
+              elif 325 <= x <= 475 and 275 <= y <= 425:
+                state = "prepare"
+                maze_type = "Kruskal"
+              elif 575 <= x <= 725 and 275 <= y <= 425:
+                state = "prepare"
+                maze_type = "Prims"
+            elif page == 2:
+              if 20 <= x <= 40 and 240 <= y <= 260:
+                page = 1
+              elif 75 <= x <= 225 and 275 <= y <= 425:
+                state="prepare"
+                maze_type = "Eller"
+              elif 325 <= x <= 475 and 275 <= y <= 425:
+                state = "prepare"
+                maze_type = "HuntKill"
+              elif 575 <= x <= 725 and 275 <= y <= 425:
+                state = "prepare"
+                maze_type = "Binary"
+    
     screen.fill(GRAY)
+
+    if state == "menu":
+      draw_menu(page)
+    elif state == "prepare":
+        grid_cell = [Cell(col, row, TILE, screen, cols, rows) for row in range(rows) for col in range(cols)]
+
+        if maze_type == "DFS":
+          current_cell = grid_cell[0]
+          stack = []
+        elif maze_type == "Kruskal":
+          current_cell = grid_cell[0]
+          set_cell = [set([i]) for i in range(len(grid_cell))]
+        elif maze_type == "Prims":
+          current_cell = grid_cell[rows*cols//2 + cols//2]
+          current_cell.visited = True
+
+        elif maze_type == "Eller":
+          current_cell = grid_cell[0]
+          count = 0
+          set_cell = []
+        elif maze_type == "HuntKill":
+          current_cell = grid_cell[0]
+        elif maze_type == "Binary":
+          current_cell = grid_cell[0]
+        state = "maze"
+
     
-  
-    #current_cell, stack = RanDFS(current_cell, stack, grid_cell)
-  
-    #RanPrims(grid_cell)
-  
-    #set_cell = Eller(grid_cell, rows, cols, count, set_cell)
-    #count += 1
-  
-    #current_cell = HuntKill(current_cell, grid_cell)
-  
-    #current_cell = BinaryTree(current_cell, grid_cell, cols)
-    
-    #set_cell = RanKruskal(grid_cell, list(set_cell))
-    #[cells.draw() for cells in grid_cell]
+    elif state == "maze":
+      [cells.draw() for cells in grid_cell]
+      if maze_type == "DFS":
+        current_cell, stack = RanDFS(current_cell, stack, grid_cell)
+      elif maze_type == "Kruskal":
+        set_cell = RanKruskal(grid_cell, list(set_cell))
+      elif maze_type == "Prims":
+        RanPrims(grid_cell)
+      elif maze_type == "Eller":
+        set_cell = Eller(grid_cell, rows, cols, count, set_cell)
+        count += 1
+      elif maze_type == "HuntKill":
+        current_cell = HuntKill(current_cell, grid_cell)
+      elif maze_type == "Binary":
+        current_cell = BinaryTree(current_cell, grid_cell, cols)
 
 
-    draw_menu()
+    
+
     pygame.display.flip()
+    
+  
   
     clock.tick(FPS)
 
