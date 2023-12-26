d6ad2263248b23debf9c9930204bebd459f9a7bb
LuongVu307 <132726554+LuongVu307@users.noreply.github.com>
2023-12-26 21:44:08 +0000

    Update generator.py

--- generator.py
+++ generator.py
@@ -24,39 +24,58 @@ clock = pygame.time.Clock()
 
 
 def draw_menu(page):
-  
+
   screen.fill(WHITE)
 
   button = pygame.Rect(0, 0, 20, 20)
   if page == 1:
-    button.center = (GUI_WIDTH-30, GUI_HEIGHT//2)
+    button.center = (GUI_WIDTH - 30, GUI_HEIGHT // 2)
   elif page == 2:
-    button.center = (30, GUI_HEIGHT//2)
+    button.center = (30, GUI_HEIGHT // 2)
   pygame.draw.rect(screen, pygame.Color("black"), button)
-  
+
   font = pygame.font.Font(None, 36)
   title_text = font.render("Select Maze Generation Algorithm", True, BLACK)
   screen.blit(title_text, (GUI_WIDTH // 2 - title_text.get_width() // 2, 50))
-  
+
   option1 = ["DFS Maze", "Kruskal's Maze", "Prims Maze"]
   option2 = ["Eller Maze", "HuntKill Maze", "Binary Tree Maze"]
   option_width = 150
 
   options = option1 if page == 1 else option2
-  
-  for option in options:
-      option_text = font.render(option, True, BLACK)
-      text_rect = option_text.get_rect(center=(option_width, GUI_HEIGHT//2))
-      choice = pygame.Rect(0, 0, 150, 150)
-      choice.center = (option_width, GUI_HEIGHT//2 + 100)
-      pygame.draw.rect(screen, pygame.Color("black"), choice)
-      screen.blit(option_text, text_rect)
-      option_width += 250
-  
-  pygame.display.flip()
-  
-  
 
+  for option in options:
+    option_text = font.render(option, True, BLACK)
+    text_rect = option_text.get_rect(center=(option_width, GUI_HEIGHT // 2))
+    choice = pygame.Rect(0, 0, 150, 150)
+    choice.center = (option_width, GUI_HEIGHT // 2 + 100)
+    pygame.draw.rect(screen, pygame.Color("black"), choice)
+    screen.blit(option_text, text_rect)
+    option_width += 250
+
+  pygame.display.flip()
+
+
+def draw_text(screen, text, font, color, position):
+  text_surface = font.render(text, True, color)
+  text_rect = text_surface.get_rect()
+  text_rect.center = position
+  screen.blit(text_surface, text_rect)
+
+
+def create_button(screen, rect, text, font, normal_color, hover_color,
+                  text_color):
+  button_hovered = rect.collidepoint(pygame.mouse.get_pos())
+  pygame.draw.rect(screen, hover_color if button_hovered else normal_color,
+                   rect)
+  text_surface = font.render(text, True, text_color)
+  text_rect = text_surface.getrect()
+  screen.blit(text_surface, text_rect)
+
+  if button_hovered and pygame.mouse.get_pressed()[0]:
+    return True
+
+  return False
 
 
 def main():
@@ -72,13 +91,12 @@ def main():
       elif event.type == pygame.MOUSEBUTTONDOWN:
         if event.button == 1:
           x, y = pygame.mouse.get_pos()
-          print(x, y)
           if state == "menu":
             if page == 1:
               if 760 <= x <= 780 and 240 <= y <= 260:
                 page = 2
               elif 75 <= x <= 225 and 275 <= y <= 425:
-                state="prepare"
+                state = "prepare"
                 maze_type = "DFS"
               elif 325 <= x <= 475 and 275 <= y <= 425:
                 state = "prepare"
@@ -90,7 +108,7 @@ def main():
               if 20 <= x <= 40 and 240 <= y <= 260:
                 page = 1
               elif 75 <= x <= 225 and 275 <= y <= 425:
-                state="prepare"
+                state = "prepare"
                 maze_type = "Eller"
               elif 325 <= x <= 475 and 275 <= y <= 425:
                 state = "prepare"
@@ -98,37 +116,45 @@ def main():
               elif 575 <= x <= 725 and 275 <= y <= 425:
                 state = "prepare"
                 maze_type = "Binary"
-    
+
     screen.fill(GRAY)
 
     if state == "menu":
       draw_menu(page)
     elif state == "prepare":
-        grid_cell = [Cell(col, row, TILE, screen, cols, rows) for row in range(rows) for col in range(cols)]
+      grid_cell = [
+          Cell(col, row, TILE, screen, cols, rows) for row in range(rows)
+          for col in range(cols)
+      ]
 
-        if maze_type == "DFS":
-          current_cell = grid_cell[0]
-          stack = []
-        elif maze_type == "Kruskal":
-          current_cell = grid_cell[0]
-          set_cell = [set([i]) for i in range(len(grid_cell))]
-        elif maze_type == "Prims":
-          current_cell = grid_cell[rows*cols//2 + cols//2]
-          current_cell.visited = True
+      if maze_type == "DFS":
+        current_cell = grid_cell[0]
+        stack = []
+      elif maze_type == "Kruskal":
+        current_cell = grid_cell[0]
+        set_cell = [set([i]) for i in range(len(grid_cell))]
+      elif maze_type == "Prims":
+        current_cell = grid_cell[rows * cols // 2 + cols // 2]
+        current_cell.visited = True
 
-        elif maze_type == "Eller":
-          current_cell = grid_cell[0]
-          count = 0
-          set_cell = []
-        elif maze_type == "HuntKill":
-          current_cell = grid_cell[0]
-        elif maze_type == "Binary":
-          current_cell = grid_cell[0]
-        state = "maze"
+      elif maze_type == "Eller":
+        current_cell = grid_cell[0]
+        count = 0
+        set_cell = []
+      elif maze_type == "HuntKill":
+        current_cell = grid_cell[0]
+      elif maze_type == "Binary":
+        current_cell = grid_cell[0]
+      state = "maze"
+      start_time = pygame.time.get_ticks()
 
-    
     elif state == "maze":
       [cells.draw() for cells in grid_cell]
+      draw_text(screen, "TIME: ", pygame.font.Font(None, 24),
+                pygame.Color("Black"), (40, 30))
+      draw_text(screen, "{:.2f}".format(
+          (pygame.time.get_ticks() - start_time) / 1000),
+                pygame.font.Font(None, 24), pygame.Color("Black"), (100, 30))
       if maze_type == "DFS":
         current_cell, stack = RanDFS(current_cell, stack, grid_cell)
       elif maze_type == "Kruskal":
@@ -143,16 +169,11 @@ def main():
       elif maze_type == "Binary":
         current_cell = BinaryTree(current_cell, grid_cell, cols)
 
-
-    
-
     pygame.display.flip()
-    
-  
-  
-    clock.tick(FPS)
 
+    clock.tick(FPS)
 
 
 if __name__ == "__main__":
   main()
+
