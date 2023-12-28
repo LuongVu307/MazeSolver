7a1d0fdb99ea40e25faccb69dadda0aeb19323bf
LuongVu307 <132726554+LuongVu307@users.noreply.github.com>
2023-12-28 01:46:47 +0000

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -1,193 +1,296 @@
 import pygame
-import sys
-from algorithm import Cell, RanDFS, RanPrims, Eller, HuntKill, BinaryTree, RanKruskal
-
-pygame.init()
-
-GUI_WIDTH, GUI_HEIGHT = 800, 500
-WIDTH, HEIGHT = 402, 302
-TILE = 10
-rows, cols = HEIGHT // TILE, WIDTH // TILE
-
-FPS = 60
-
-screen = pygame.display.set_mode((GUI_WIDTH, GUI_HEIGHT))
-pygame.display.set_caption("Maze Generator")
-
-BLACK = (0, 0, 0)
-WHITE = (250, 250, 250)
-GRAY = (50, 50, 50)
-
-clock = pygame.time.Clock()
+import random
 
 
-def draw_menu(page):
+class Cell:
 
-  screen.fill(WHITE)
+  def __init__(self, x, y, TILE, screen, cols, rows):
+    self.TILE = TILE
+    self.screen = screen
+    self.cols = cols
+    self.rows = rows
+    self.visited = False
+    self.bias_x, self.bias_y = 50, 100
+    self.x, self.y = x, y
+    self.walls = {"right": True, "left": True, "top": True, "bottom": True}
 
-  button = pygame.Rect(0, 0, 20, 20)
-  if page == 1:
-    button.center = (GUI_WIDTH - 30, GUI_HEIGHT // 2)
-  elif page == 2:
-    button.center = (30, GUI_HEIGHT // 2)
-  pygame.draw.rect(screen, pygame.Color("black"), button)
+  def draw(self):
+    x, y = self.x * self.TILE, self.TILE * self.y
 
-  font = pygame.font.Font(None, 36)
-  title_text = font.render("Select Maze Generation Algorithm", True, BLACK)
-  screen.blit(title_text, (GUI_WIDTH // 2 - title_text.get_width() // 2, 50))
+    x += self.bias_x
+    y += self.bias_y
 
-  option1 = ["DFS Maze", "Kruskal's Maze", "Prims Maze"]
-  option2 = ["Eller Maze", "HuntKill Maze", "Binary Tree Maze"]
-  option_width = 150
+    if self.visited:
+      pygame.draw.rect(self.screen, pygame.Color("black"),
+                       (x, y, self.TILE, self.TILE))
 
-  options = option1 if page == 1 else option2
+    if self.walls["right"]:
+      pygame.draw.line(self.screen, pygame.Color("darkorange"),
+                       (x + self.TILE, y), (x + self.TILE, y + self.TILE), 1)
+    if self.walls["left"]:
+      pygame.draw.line(self.screen, pygame.Color("darkorange"),
+                       (x, y + self.TILE), (x, y), 1)
+    if self.walls["top"]:
+      pygame.draw.line(self.screen, pygame.Color("darkorange"), (x, y),
+                       (x + self.TILE, y), 1)
+    if self.walls["bottom"]:
+      pygame.draw.line(self.screen, pygame.Color("darkorange"),
+                       (x + self.TILE, y + self.TILE), (x, y + self.TILE), 1)
 
-  for option in options:
-    option_text = font.render(option, True, BLACK)
-    text_rect = option_text.get_rect(center=(option_width, GUI_HEIGHT // 2))
-    choice = pygame.Rect(0, 0, 150, 150)
-    choice.center = (option_width, GUI_HEIGHT // 2 + 100)
-    pygame.draw.rect(screen, pygame.Color("black"), choice)
-    screen.blit(option_text, text_rect)
-    option_width += 250
+  def draw_current(self):
+    x, y = self.x * self.TILE, self.TILE * self.y
 
-  pygame.display.flip()
+    x += self.bias_x
+    y += self.bias_y
+
+    pygame.draw.rect(self.screen, pygame.Color("brown"),
+                     (x, y, self.TILE, self.TILE))
+
+  def check_cell(self, x, y, grid_cell):
+    find_index = lambda x, y: x + y * self.cols
+
+    if x < 0 or x > self.cols - 1 or y < 0 or y > self.rows - 1:
+      return False
+
+    return grid_cell[find_index(x, y)]
+
+  def check_neighbor(self, grid_cell):
+    neighbor = []
+    top = self.check_cell(self.x, self.y - 1, grid_cell)
+    right = self.check_cell(self.x + 1, self.y, grid_cell)
+    bottom = self.check_cell(self.x, self.y + 1, grid_cell)
+    left = self.check_cell(self.x - 1, self.y, grid_cell)
+
+    if top and not top.visited:
+      neighbor.append(top)
+    if right and not right.visited:
+      neighbor.append(right)
+    if bottom and not bottom.visited:
+      neighbor.append(bottom)
+    if left and not left.visited:
+      neighbor.append(left)
+
+    return random.choice(neighbor) if neighbor else False
 
 
-def draw_text(screen, text, font, color, position):
-  text_surface = font.render(text, True, color)
-  text_rect = text_surface.get_rect()
-  text_rect.left = position[1]
-  text_rect.centery = position[0]
-  screen.blit(text_surface, text_rect)
+def rm_walls(current, next):
+  dx = current.x - next.x
+  if dx == 1:
+    current.walls["left"] = False
+    next.walls["right"] = False
+  elif dx == -1:
+    current.walls["right"] = False
+    next.walls["left"] = False
+
+  dy = current.y - next.y
+
+  if dy == 1:
+    current.walls["top"] = False
+    next.walls["bottom"] = False
+  elif dy == -1:
+    current.walls["bottom"] = False
+    next.walls["top"] = False
 
 
-def create_button(screen, rect, text, font, normal_color, hover_color,
-                  text_color):
-  button_hovered = rect.collidepoint(pygame.mouse.get_pos())
-  pygame.draw.rect(screen, hover_color if button_hovered else normal_color,
-                   rect)
-  text_surface = font.render(text, True, text_color)
-  text_rect = text_surface.getrect()
-  screen.blit(text_surface, text_rect)
+def RanDFS(current_cell, stack, grid_cell):
+  current_cell.visited = True
+  current_cell.draw_current()
 
-  if button_hovered and pygame.mouse.get_pressed()[0]:
-    return True
+  next_cell = current_cell.check_neighbor(grid_cell)
+  if next_cell:
+    next_cell.visited = True
+    stack.append(current_cell)
+    rm_walls(current_cell, next_cell)
+    current_cell = next_cell
+  elif stack:
+    current_cell = stack.pop()
 
-  return False
+  return current_cell, stack
 
 
-def main():
-  running = True
-  state = "menu"
-  page = 1
-  maze_type = None
-  while running:
-    for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        running = False
-        sys.exit()
-      elif event.type == pygame.MOUSEBUTTONDOWN:
-        if event.button == 1:
-          x, y = pygame.mouse.get_pos()
-          if state == "menu":
-            if page == 1:
-              if 760 <= x <= 780 and 240 <= y <= 260:
-                page = 2
-              elif 75 <= x <= 225 and 275 <= y <= 425:
-                state = "prepare"
-                maze_type = "DFS"
-              elif 325 <= x <= 475 and 275 <= y <= 425:
-                state = "prepare"
-                maze_type = "Kruskal"
-              elif 575 <= x <= 725 and 275 <= y <= 425:
-                state = "prepare"
-                maze_type = "Prims"
-            elif page == 2:
-              if 20 <= x <= 40 and 240 <= y <= 260:
-                page = 1
-              elif 75 <= x <= 225 and 275 <= y <= 425:
-                state = "prepare"
-                maze_type = "Eller"
-              elif 325 <= x <= 475 and 275 <= y <= 425:
-                state = "prepare"
-                maze_type = "HuntKill"
-              elif 575 <= x <= 725 and 275 <= y <= 425:
-                state = "prepare"
-                maze_type = "Binary"
+def RanPrims(grid_cell):
+  ls = []
 
-    screen.fill(GRAY)
+  for cell in grid_cell:
+    if cell.visited is True:
+      ls.append(cell)
+  if len(ls) == len(grid_cell):
+    return False
 
-    if state == "menu":
-      draw_menu(page)
-    elif state == "prepare":
-      grid_cell = [
-          Cell(col, row, TILE, screen, cols, rows) for row in range(rows)
-          for col in range(cols)
-      ]
+  while True:
+    chosen_cell = random.choice(ls)
+    next_cell = chosen_cell.check_neighbor(grid_cell)
+    if next_cell and next_cell.visited == False:
+      next_cell.visited = True
+      rm_walls(chosen_cell, next_cell)
+      next_cell.draw_current()
+      break
 
-      if maze_type == "DFS":
-        current_cell = grid_cell[0]
-        stack = []
-      elif maze_type == "Kruskal":
-        current_cell = grid_cell[0]
-        set_cell = [set([i]) for i in range(len(grid_cell))]
-      elif maze_type == "Prims":
-        current_cell = grid_cell[rows * cols // 2 + cols // 2]
-        current_cell.visited = True
+  return grid_cell
 
-      elif maze_type == "Eller":
-        current_cell = grid_cell[0]
-        count = 0
-        set_cell = []
-      elif maze_type == "HuntKill":
-        current_cell = grid_cell[0]
-      elif maze_type == "Binary":
-        current_cell = grid_cell[0]
-      state = "maze"
-      start_time = pygame.time.get_ticks()
-      steps = 0
-    elif state == "maze":
-      steps += 1
-      [cells.draw() for cells in grid_cell]
-      draw_text(screen,
-                f"TIME: {(pygame.time.get_ticks() - start_time) / 1000}",
-                pygame.font.Font(None, 24), pygame.Color("Red"), (40, 30))
-      draw_text(screen, f"STEPS: {steps}", pygame.font.Font(None, 24),
-                pygame.Color("Red"), (70, 30))
-      if maze_type == "DFS":
-        current_cell, stack = RanDFS(current_cell, stack, grid_cell)
-        if not stack:
-          state = "end maze"
-      elif maze_type == "Kruskal":
-        set_cell = RanKruskal(grid_cell, list(set_cell))
-        print(set_cell)
-        if not set_cell:
-          state = "end maze"
-      elif maze_type == "Prims":
-        if RanPrims(list(grid_cell)):
-          grid_cell = RanPrims(list(grid_cell))
+
+def Eller(grid_cell, rows, cols, count, set_cell):
+  if cols * count < len(grid_cell):
+
+    i = cols * count
+    ls = []
+    while i < cols * (count + 1) - 1:
+      cells = set()
+      if random.randint(1, 10) > 5:
+        rm_walls(grid_cell[i], grid_cell[i + 1])
+        cells.add(i)
+        cells.add(i + 1)
+      else:
+        cells.add(i)
+
+      if count > 0:
+        if random.randrange(1, 10) > 5:
+          rm_walls(grid_cell[i], grid_cell[i - cols])
+          cells.add(i)
+          cells.add(i - cols)
         else:
-          print("No Path")
-          state = "end maze"
-      elif maze_type == "Eller":
-        set_cell = Eller(grid_cell, rows, cols, count, set_cell)
+          cells.add(i)
+
+      ls.append(cells)
+      i += 1
+    num = 0
+    for i in ls:
+      for j in i:
+        num += 1
+    if num != cols * (count + 1):
+      ls.append(set([cols * (count + 1) - 1]))
+
+    merged_sets = []
+
+    for set1 in set_cell:
+      merged = False
+
+      for merged_set in merged_sets:
+        if any(element in merged_set for element in set1):
+          merged_set.update(set1)
+          merged = True
+          break
+
+      if not merged:
+        merged_sets.append(set1.copy())
+
+    for set2 in ls:
+      merged = False
+
+      for merged_set in merged_sets:
+        if any(element in merged_set for element in set2):
+          merged_set.update(set2)
+          merged = True
+          break
+
+      if not merged:
+        merged_sets.append(set2.copy())
+
+    return merged_sets
+
+  else:
+    ls = set_cell.copy()
+    while True:
+      num_set = random.randint(0, len(ls) - 1)
+      set_chosen = ls[num_set]
+
+      num_chosen = random.choice(list(set_chosen))
+      cell_chosen = grid_cell[num_chosen]
+
+      next_cell = cell_chosen.check_neighbor(grid_cell)
+      if next_cell and grid_cell.index(next_cell) not in set_chosen:
+        rm_walls(cell_chosen, next_cell)
+        next_cell.draw_current()
+        set_chosen.add(grid_cell.index(next_cell))
+        break
+
+    merged_sets = []
+
+    for new_set in ls:
+      sets_to_merge = []
+
+      for existing_set in merged_sets:
+        if any(element in existing_set for element in new_set):
+          sets_to_merge.append(existing_set)
+
+      if sets_to_merge:
+        merged_set = set().union(new_set, *sets_to_merge)
+        merged_sets = [s for s in merged_sets if s not in sets_to_merge]
+        merged_sets.append(merged_set)
+      else:
+        merged_sets.append(new_set.copy())
+
+    if len(merged_sets) == 1:
+      return False
+    return merged_sets
+
+
+def HuntKill(current_cell, grid_cell):
+  print(current_cell)
+  current_cell.visited = True
+
+  current_cell.draw_current()
+
+  next_cell = current_cell.check_neighbor(grid_cell)
+  count = 0
+  if next_cell:
+    next_cell.visited = True
+    rm_walls(current_cell, next_cell)
+    current_cell = next_cell
+  else:
+    for i in range(len(grid_cell)):
+      #grid_cell[i].draw_current()
+      if grid_cell[i].visited is True:
         count += 1
-        if not set_cell:
-          state = "end maze"
-      elif maze_type == "HuntKill":
-        current_cell = HuntKill(current_cell, grid_cell)
-        if current_cell == False:
-          maze_type = "end maze"
-      elif state == "Binary":
-        current_cell = BinaryTree(current_cell, grid_cell, cols)
-    elif state == "end maze":
-      pass
+        if grid_cell[i].check_neighbor(grid_cell):
+          current_cell = grid_cell[i]
+          break
+      print(count)
 
-    pygame.display.flip()
+  if count >= len(grid_cell) - 1:
+    return False
 
-    clock.tick(FPS)
+  return current_cell
 
 
-if __name__ == "__main__":
-  main()
+def BinaryTree(current_cell, grid_cell, cols):
+  current_cell.visited = True
+  if random.randint(1, 10) > 3:
+    current_cell.walls["left"] = False
+  if random.randint(1, 10) > 3:
+    if grid_cell.index(current_cell) > cols:
+      current_cell.walls["top"] = False
+  current_cell.draw_current()
+  if grid_cell.index(current_cell) < len(grid_cell) - 1:
+    current_cell = grid_cell[grid_cell.index(current_cell) + 1]
+  print(current_cell)
+  return current_cell
+
+
+def RanKruskal(grid_cell, set_cell):
+  while True:
+    choice = random.randint(0, len(grid_cell) - 1)
+    current_cell = grid_cell[choice]
+    next_cell = current_cell.check_neighbor(grid_cell)
+    next_choice = grid_cell.index(next_cell)
+    first, second = None, None
+    for i in range(len(set_cell)):
+      if choice in set_cell[i]:
+        first = i
+      if next_choice in set_cell[i]:
+        second = i
+
+    if first != second:
+      current_cell.draw_current()
+      next_cell.draw_current()
+      rm_walls(current_cell, next_cell)
+      new_cell = set_cell[first] | set_cell[second]
+      set_cell[first] = new_cell
+      set_cell.pop(second)
+      break
+
+    if len(set_cell) == len(grid_cell):
+      return False
+
+  return set_cell
+
